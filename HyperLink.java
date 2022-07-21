eaffdcebeed6d8f0526ae136b3f17180a6172ac3
shitallotke <108998809+shitallotke@users.noreply.github.com>
2022-07-21 09:56:37 -0500

    Add files via upload

--- HyperLink.java
+++ HyperLink.java
@@ -21,16 +21,20 @@ public class HyperLink {
 		leaf.manage().window().maximize();
 		leaf.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		
+		   
+		 //Go to Home Page
+		 leaf.findElement(By.xpath("//a[text()='Go to Home Page']")).click();
+		 
+		 leaf.findElement(By.xpath("//*[@id=\"post-153\"]/div[2]/div/ul/li[3]/a/img")).click(); //Navigate to link.html 
+		   	
+		
 		//Find where am supposed to go without clicking me?
 		System.out.println(leaf.findElement(By.linkText("Find where am supposed to go without clicking me?")).getAttribute("href"));
-		
-		//Go to Home Page
-		leaf.findElement(By.xpath("//a[text()='Go to Home Page']")).click();
-		
+				
 		//How many links are available in this page?
 		List<WebElement> links = leaf.findElements(By.tagName("a"));
 		int count = links.size();
-	    System.out.println("The total number of links is "+count);
+	    System.out.println("The total number of links is : "+count);
 	    
 	    //Display all the available link
 	   for(WebElement allink : links)
@@ -38,9 +42,15 @@ public class HyperLink {
 	    	System.out.println(allink.getText() + " - " + allink.getAttribute("href"));
 	    }
 	   
-	   	//Verify am I broken?
-	 //  List<WebElement> link = leaf.findElements(By.tagName("a"));
-	  
+	 //Verify am I broken?*/
+		 leaf.findElement(By.xpath("//a[contains(text(),'broken?')]")).click();
+		 String text = leaf.findElement(By.tagName("h1")).getText();
+		 System.out.println(text);
+		 
+		 
+		 
+		  
+	
 	}
 	
 
